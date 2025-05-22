1a388f0681e9608dd65d0430858456b19e10d659
Jnanesh2425 <jnaneshnr824@gmail.com>
2025-05-22 08:06:19 +0530

    Added routing for skill practice feature card

--- Flacipy/src/App.jsx
+++ Flacipy/src/App.jsx
@@ -3,14 +3,14 @@ import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-route
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import Navbar from './Components/Navbar/Navbar';
+import Navbar from './Components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Herosec from './pages/Herosec/Herosec';
 import Mock from './pages/Mock';
 import SkillPractice from './pages/SkillPractice/SkillPractice';
 
-
+// Wrapper to access location inside Routes
 const Layout = () => {
   const location = useLocation();
   const isHome = location.pathname === '/';
