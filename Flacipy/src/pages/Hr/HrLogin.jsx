2a15c0cc9c9185934627a08aa5ec24e1239dabe7
ipraveendn <praveendn1230@gmail.com>
2025-07-12 12:47:10 +0530

    import stats 2

--- Flacipy/src/pages/Hr/HrLogin.jsx
+++ Flacipy/src/pages/Hr/HrLogin.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { toast } from 'sonner';
 import { useNavigate } from 'react-router-dom';
-import { API_BASE_URL } from '../../config';
+import  API_BASE_URL  from '../../config/api';
 
 const HrLogin = () => {
     const navigate = useNavigate();
